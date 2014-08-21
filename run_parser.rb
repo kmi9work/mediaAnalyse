efe00b1bf4d730c549794022da0366d5ae3c78c1
Michael <kmi9.other@gmail.com>
2014-08-21 15:48:11 +0400

    msystem2

--- run_parser.rb
+++ run_parser.rb
@@ -1,5 +1,5 @@
   require 'daemons'
   
   Daemons.run_proc('parser') do
-    `cd /home/web/public_html/msystem && export PATH=/usr/local/jdk/bin:/usr/local/rvm/gems/ruby-2.1.2/bin:/usr/local/rvm/gems/ruby-2.1.2@global/bin:/usr/local/rvm/rubies/ruby-2.1.2/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/rvm/bin:/usr/local/bin:/usr/X11R6/bin:/root/bin && RAILS_ENV=production rake parse:rp5`
+    `cd /home/web/public_html/msystem2 && export PATH=/usr/local/jdk/bin:/usr/local/rvm/gems/ruby-2.1.2/bin:/usr/local/rvm/gems/ruby-2.1.2@global/bin:/usr/local/rvm/rubies/ruby-2.1.2/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/rvm/bin:/usr/local/bin:/usr/X11R6/bin:/root/bin && RAILS_ENV=production rake parse:rp5`
   end
