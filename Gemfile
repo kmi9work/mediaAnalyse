f3677dfcb1785e3a009f69ec445aa82cf227d7e8
Michael <kmi9.other@gmail.com>
2014-10-09 12:00:20 +0400

    deploy

--- Gemfile
+++ Gemfile
@@ -1,63 +1,50 @@
 source 'https://rubygems.org'
-gem 'rails', '4.0.0'
-gem 'sass-rails', '~> 4.0.0'
+ruby '2.1.3'
+gem 'rails', '4.1.6'
+gem 'sass-rails'
 
 # Use Uglifier as com	essor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
-gem 'coffee-rails', '~> 4.0.0'
+gem 'uglifier'
+gem 'coffee-rails'
 
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-# gem 'therubyracer', platforms: :ruby
+gem 'execjs'
+gem 'therubyracer', platforms: :ruby
 
 gem 'jquery-rails'
 
 gem 'turbolinks'
-gem 'jbuilder', '~> 1.2'
-group :doc do
-  # bundle exec rake doc:rails generates the API under doc/api.
-  gem 'sdoc', require: false
-end
+gem 'jbuilder'
+
 
 # mygems
 
-group :production do
-	gem 'mysql2'
-  gem 'feedjira'
-end
-group :development, :test do
-	# gem 'sqlite3'
-	gem 'pg'
-end
+gem 'pg'
 
-
+gem 'feedjira'
 gem 'nokogiri'
 gem 'daemons'
 gem 'daemons-rails'
 gem 'sorcery'
 gem 'logger'
 gem 'kaminari'
-gem 'sunspot_rails', '2.0.0'
-gem 'sunspot_solr', '2.0.0'
+# gem 'sunspot_rails', '2.0.0'
+# gem 'sunspot_solr', '2.0.0', group: :development
+gem 'will_paginate'
 gem 'progress_bar'
+
+#deploy
+group :production do
+	gem 'unicorn'
+	gem 'unicorn-rails'
+end
+gem 'capistrano', '~> 2.15'
+gem "rvm-capistrano"
+# gem 'bundler-capistrano'
+
 
 # gem 'myaso', :git => 'https://github.com/dmchk/myaso.git'
 
 gem 'selenium-webdriver'
 gem 'headless'
 gem 'net-ssh-gateway'
-
-# gem 'pg'
-# gem 'websocket-rails'
-
-# Use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-gem 'capistrano', group: :development
-
-# Use debugger
-# gem 'debugger', group: [:development, :test]
-
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,96 +1,46 @@
-require 'net/ssh/gateway'
+require 'rvm/capistrano' # Для работы rvm
+require 'bundler/capistrano' # Для работы bundler. При изменении гемов bundler автоматически обновит все гемы на сервере, чтобы они в точности соответствовали гемам разработчика. 
 
-class Net::SSH::Gateway 
-  # Opens a SSH tunnel from a port on a remote host to a given host and port 
-  # on the local side 
-  # (equivalent to openssh -R parameter) 
-  def open_remote(port, host, remote_port, remote_host = "127.0.0.1") 
-    ensure_open! 
+set :application, "rsser"
+set :rails_env, "production"
+set :domain, "michael@31.24.30.28" # Это необходимо для деплоя через ssh. Именно ради этого я настоятельно советовал сразу же залить на сервер свой ключ, чтобы не вводить паролей.
+set :deploy_to, "/srv/#{application}"
+set :use_sudo, false
+set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
-    @session_mutex.synchronize do 
-      @session.forward.remote(port, host, remote_port, remote_host) 
-    end 
+set :rvm_ruby_string, :local # Это указание на то, какой Ruby интерпретатор мы будем использовать.
 
-    if block_given? 
-      begin 
-        yield [remote_port, remote_host] 
-      ensure 
-        close_remote(remote_port, remote_host) 
-      end 
-    else 
-      return [remote_port, remote_host] 
-    end 
-  rescue Errno::EADDRINUSE 
-    retry 
-  end 
+set :scm, :git # Используем git. Можно, конечно, использовать что-нибудь другое - svn, например, но общая рекомендация для всех кто не использует git - используйте git. 
+set :repository,  "git@github.com:kmi9work/mediaAnalyse.git" # Путь до вашего репозитария. Кстати, забор кода с него происходит уже не от вас, а от сервера, поэтому стоит создать пару rsa ключей на сервере и добавить их в deployment keys в настройках репозитария.
+set :branch, "rss" # Ветка из которой будем тянуть код для деплоя.
+set :deploy_via, :remote_cache # Указание на то, что стоит хранить кеш репозитария локально и с каждым деплоем лишь подтягивать произведенные изменения. Очень актуально для больших и тяжелых репозитариев.
 
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
 
-  # Cancels port-forwarding over an open port that was previously opened via 
-  # open_remote. 
-  def close_remote(port, host = "127.0.0.1") 
-    ensure_open! 
+before 'deploy:setup', 'rvm:install_rvm', 'rvm:install_ruby' # интеграция rvm с capistrano настолько хороша, что при выполнении cap deploy:setup установит себя и указанный в rvm_ruby_string руби.
 
-    @session_mutex.synchronize do 
-      @session.forward.cancel_remote(port, host) 
-    end 
-  end 
+after 'deploy:update_code', :roles => :app do
+  # Здесь для примера вставлен только один конфиг с приватными данными - database.yml. Обычно для таких вещей создают папку /srv/myapp/shared/config и кладут файлы туда. При каждом деплое создаются ссылки на них в нужные места приложения.
+  puts "ololo"
+  run "rm -f #{current_release}/config/database.yml"
+  run "ln -s #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
+
+  run "chmod +x #{current_release}/lib/daemons/rsser_ctl"
 end
-=begin
-# config valid only for Capistrano 3.1
-lock '3.2.1'
 
-set :application, 'msystem'
-set :repo_url, 'git@github.com:kmi9work/mediaAnalyse.git'
-
-set :deploy_to, '/home/web/public_html/msystem'
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/usr/local/rvm/gems/ruby-2.1.2/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-set :user, 'root'
-# set :rails_env, "production"
-set :deploy_via, :copy
-
-# 
-=end
-
-
-server "vps45162.vps.tech-logol.ru", :app, :web, :db, :primary => true
-
-
+# Далее идут правила для перезапуска unicorn. Их стоит просто принять на веру - они работают.
+# В случае с Rails 3 приложениями стоит заменять bundle exec unicorn_rails на bundle exec unicorn
 namespace :deploy do
-  task :remote_tunnel do
-    gateway = Net::SSH::Gateway.new(
-      'vps45162.vps.tech-logol.ru'
-    )
-    port = gateway.open('127.0.0.1', 9150, 9150)    
+  task :restart do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D; fi"
+  end
+  task :start do
+    run "bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
+  end
+  task :stop do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
   end
 end
-
-task :hello do
-
-end
-
-
-# before "deploy:update_code", "deploy:remote_tunnel"
-
-before "hello", "deploy:remote_tunnel"
-
