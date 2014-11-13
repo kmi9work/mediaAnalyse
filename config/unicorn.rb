d65ecef81081fb53755fc2240a710553ed1e7a7e
Michael <kmi9.other@gmail.com>
2014-11-13 12:24:55 +0300

    timeout bigger

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,8 +6,8 @@ log_file   = "#{rails_root}/log/unicorn.log"
 err_log    = "#{rails_root}/log/unicorn_error.log"
 old_pid    = pid_file + '.oldbin'
 
-timeout 30
-worker_processes 1 
+timeout 120
+worker_processes 1
 listen socket_file, :backlog => 1024
 pid pid_file
 stderr_path err_log
