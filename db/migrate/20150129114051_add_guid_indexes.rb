71659342a71d3434a5ff4bc08dbd25fe88591a2f
Michael <kmi9.other@gmail.com>
2015-01-29 14:51:18 +0300

    next

--- db/migrate/20150129114051_add_guid_indexes.rb
+++ db/migrate/20150129114051_add_guid_indexes.rb
@@ -1,6 +1,6 @@
 class AddGuidIndexes < ActiveRecord::Migration
   def change
     add_index :texts, :guid, :name => 'texts_guid_ix'
-    add_index :texts, [:origin_id, :guid], :name => 'texts_guid_ix'
+    add_index :texts, [:origin_id, :guid], :name => 'texts_origin_id_guid_ix'
   end
 end
